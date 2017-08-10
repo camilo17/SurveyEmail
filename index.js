b285b4f20be5bb1b33b1d0e0e1abb7e14b530c86
camilo17 <cami172013@gmail.com>
2017-08-10 08:59:06 -0500

    Added cookieSession and configured it.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); 
 const authRoutes = require('./routes/authRoutes'); 
 const keys = require('./config/keys');
 require('./models/User'); 
@@ -10,6 +11,14 @@ mongoose.connect(keys.mongoURI);
 
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+
 authRoutes(app); 
 const PORT = process.env.PORT || 5000; 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,10 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 module.exports = (app) => {
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
 
-app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get('/auth/google/callback', passport.authenticate('google'))
 }; 
