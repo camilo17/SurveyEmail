4b2401f48c03cb65a81dfc7727fb6843cdd7a1a9
camilo17 <cami172013@gmail.com>
2017-08-10 08:15:01 -0500

    Created models directory and created mongoDB User model.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport'); 
 const authRoutes = require('./routes/authRoutes'); 
 const keys = require('./config/keys');
+require('./models/User'); 
+require('./services/passport'); 
 
 
 mongoose.connect(keys.mongoURI);
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,31 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; 
 const keys = require('../config/keys'); 
+const mongoose = require('mongoose'); 
 
-
+const User = mongoose.model('users'); 
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleSecretID,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken,); 
+
+    User.findOne({googleId: profile.id})
+        .then((existingUser) => {
+            if(existingUser) {
+                 done(null, existingUser);    
+            } else {
+                new User({googleId: profile.id})
+                .save()
+                .then(user => {
+                    done(null, user); 
+                }); 
+            }
+        })
+
+    new User({
+        googleId: profile.id
+    }).save(); 
 })
 ); 
