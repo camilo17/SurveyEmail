5f041ada90eb1d5e77ac2d4d1b8c3586034ca8df
camilo17 <cami172013@gmail.com>
2017-08-11 09:10:47 -0500

    tweak google strategy settings to work with heroku.

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleSecretID,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
 
     User.findOne({googleId: profile.id})
