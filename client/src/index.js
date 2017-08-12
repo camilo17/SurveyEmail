e70473b63c9aff7aec699342ccf44be03c7a4892
camilo17 <cami172013@gmail.com>
2017-08-12 09:40:47 -0500

    Added create react app and added redux and other libraries.

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './components/App'; 
+import {Provider} from 'react-redux';
+import {createStore , applyMiddleware} from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+
+
+
+ReactDOM.render(<App />, document.querySelector('#root')); 
--- config/prod.js
+++ config/prod.js
@@ -4,4 +4,4 @@ module.exports = {
     googleSecretID: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
-}
+}; 
