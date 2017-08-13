c437f3844460bd1be240bddec3dd84c4d579a1e3
camilo17 <cami172013@gmail.com>
2017-08-12 21:56:04 -0500

    Added react router to the project and did a quick implementation.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom'; 
+
+const Header = () => {
+    return (
+        <h2>
+            Header!! 
+        </h2>    
+    )
+}
+
+
 
 const App = () => {
     return (
         <div>
-            Hi There! 
+            <BrowserRouter>
+                <div>
+                    <Route path='/' component={Header}/>
+                   
+                </div>    
+
+            </BrowserRouter>
         </div>    
     )
 }
