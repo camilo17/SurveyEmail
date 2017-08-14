f9d14b8c25761d73091d19f1b6eb93302a6e95f2
camilo17 <cami172013@gmail.com>
2017-08-13 22:36:17 -0500

    made the fetchUser action using redux thunk.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 import {FETCH_USER} from './types'; 
 
 const fetchUser = () => {
-    axios.get('/api/current_user')
+    return function(dispatch) {     //Redux Thunk sees that a function is returned and passes the dispatch function to the returned function
+        axios.get('/api/current_user')
+            .then(res => dispatch({
+                type: FETCH_USER,
+                payload: res
+            }))
+    }
+
+    
 
 };
