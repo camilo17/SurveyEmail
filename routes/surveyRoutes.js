58c75b76b4552512a8ad483dd2c444f7e1cc1920
camilo17 <cami172013@gmail.com>
2017-08-23 12:12:03 -0500

    configured send grid, pain tbh.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,29 @@
+const mongoose = require('mongoose'); 
 const requireLogin = require('../middlewares/requireLogin'); 
-const requiredCredits = require('../middlewares/requiredCredits'); 
+const requiredCredits = require('../middlewares/requiredCredits');
+const Mailer = require('../services/Mailer'); 
+
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate'); 
+
+const Survey = mongoose.model('surveys'); 
 
 module.exports = (app) => {
 
     app.post('./api/surveys', requireLogin, requiredCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body; 
+
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(a => ({email: a.trim()})),
+            _user: req.user.id,
+            dateSent: Date.now()
+
+        }); 
+
+        const mailer = new Mailer(survey, template);
     }); 
 
 };
---
+++ services/Mailer.js
@@ -0,0 +1,44 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail; 
+const keys = require('../config/keys'); 
+
+class Mailer extends helper.Mail {
+    constructor({subject, recipients}, content){
+
+        super(); 
+
+        this.sgApi = ssendgrid(keys.sendGridKey); 
+        this.from_email = new helper.Email('no-relply@emaily.com'); 
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content); 
+        this.recipients = this.formatAddresses(recipients); 
+
+        this.addContent(this.body); //function from Mail 
+        this.addClickTracking(); 
+        this.addRecipients(); 
+    }       
+
+    formatAddresses(recipients){
+        return recipients.map(({email}) => {
+            return new helper.Email(email); 
+        })
+    }
+
+    addClickTracking(){
+        const trackingSetting = new helper.trackingSetting();
+        const clickTricking = new helper.ClickTracking(true, true); 
+
+        trackingSetting.setClickTracking(clickTricking); 
+        this.addTrackingSettings(trackingSetting); 
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization(); 
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize); //functionin mail class 
+    }
+}
+
+module.exports = Mailer; 
